a3cf46ca8ec1d6a7d1732af820f785b42d02039b
jakka-kaveri <jkaveri99@gmail.com>
2023-08-30 14:07:23 +0530

    feature2 & feature3 added

--- src/main/java/org/example/features/Feature1.java
+++ src/main/java/org/example/features/Feature1.java
@@ -3,5 +3,9 @@ package org.example.features;
 public class Feature1 {
     public static void main(String[] args) {
         System.out.println("feature1 is added");
+
+        System.out.println("feature2 is added");
+
+        System.out.println("feature3 is added");
     }
 }
